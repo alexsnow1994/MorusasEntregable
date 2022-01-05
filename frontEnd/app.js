42e2c2c584450af8ee63837a555f9f70c3dcd5e5
alexsnow1994 <ing.estrada.rocha@gmail.com>
2022-01-04 22:01:52 -0600

    iniciando manipulacion del DOM

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,1 +1,13 @@
 import './styles/app.css';
+
+document.getElementById('book-form')
+    .addEventListener('submit', e => {
+        let title = document.getElementById('title').value;
+        let author = document.getElementById('author').value;
+        let isbn = document.getElementById('isbn').value;
+        let image = document.getElementById('image').files;
+
+
+        console.log(title, author, isbn, image);
+        e.preventDefault();
+    });
