8c8ec52080d2adb2b8da27a9af6ee88c1b744319
alexsnow1994 <ing.estrada.rocha@gmail.com>
2022-01-06 22:40:17 -0600

    agregando dom

--- frontEnd/UI.js
+++ frontEnd/UI.js
@@ -11,7 +11,20 @@ class UI {
             let div = document.createElement('div');
             div.className = '';
             div.innerHTML = `
-             
+             <div class="cards m-2">
+                <div class="row">
+                    <div class="col-md-4>
+                    <img src="${book.imagePath}" alt="" class="img-fluid"/>
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-block px-2">
+                        <h4 class="card-title">"${book.title}"</h4>
+                        <p class="card-text">"${book.author}}"</p>
+                        <a href=# class="bt btn-danger delete"></a>  
+                    </div>
+                    </div>
+                </div>
+             </div>
             `;
         })
     };
