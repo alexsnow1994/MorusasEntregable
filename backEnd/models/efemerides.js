f17883aef020d356d416a8ee72967b91d685ac81
alexsnow1994 <ing.estrada.rocha@gmail.com>
2022-01-09 13:03:43 -0600

    previo a revert

--- backEnd/models/efemerides.js
+++ backEnd/models/efemerides.js
@@ -1,13 +1,13 @@
-const {Schema, model}= require('mongoose');
-const express = require('express')
+// const {Schema, model}= require('mongoose');
+// const express = require('express')
 
-const EfeSchema = new Schema ({
-    created_at: {type: Date, default: Date.now},
-    title: { type: String, required : true},
-    author: {type: String, required : true},
-    isbn: {type: String, required : true},
-    imagePath: {type: String}
+// const EfeSchema = new Schema ({
+//     created_at: {type: Date, default: Date.now},
+//     title: { type: String, required : true},
+//     author: {type: String, required : true},
+//     isbn: {type: String, required : true},
+//     //imagePath: {type: String}
     
-})
+// })
 
-module.exports=model('book',EfeSchema )
+// module.exports=model('book',EfeSchema )
