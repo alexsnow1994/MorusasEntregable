fd5f80f2d05c1ee06076f5c0e3b7fe08fe175054
alexsnow1994 <ing.estrada.rocha@gmail.com>
2022-01-09 14:22:43 -0600

    Revert "estable subir imagenes"
    This reverts commit 5ba7d3cfe0d4d08ae284b2901f62d0321a21fbe3.

--- backEnd/app.js
+++ backEnd/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const morgan = require('express');
 const multer = require('multer');
 const path = require('path');
-const cors = require('cors');
+const cors = require('cors')
 
 
 
